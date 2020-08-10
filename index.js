73a827aeb02d77b803c328a651db07c107dd1b7c
vivek9716 <nice.vivek25@gmail.com>
2020-08-10 10:41:39 +0530

    enable express

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-console.log("Hello Jenkins");
-console.log("This is my first step towards jenkins");
+var express = require('express');
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+	res.json([{
+		user_id: '1',
+		user_name: 'Vivek Chaudhary'
+	}]);
+});
+
+
+app.listen(PORT, () => {
+	console.log(`App is running on ${PORT}`);
+});
